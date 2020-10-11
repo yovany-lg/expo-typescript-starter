267f406a181a1c7848ab60fab9fe35c447165e22
yovany-lg <yovany.lg@gmail.com>
2020-10-11 16:42:01 -0500

    adding ESLint and Prettier

--- App.tsx
+++ App.tsx
@@ -1,15 +1,7 @@
+/* eslint-disable react/style-prop-object */
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +11,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App(): JSX.Element {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
